14899c50df24efc7f4d907416d4f4cc662e63fe8
MTristan <53493044+MTristan012@users.noreply.github.com>
2023-05-18 19:54:15 -0600

    A1.1 "Offcanvas/Main Skeleton"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,148 @@
+import { useState } from "react";
+
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import InputGroup from "react-bootstrap/InputGroup";
+import ListGroup from "react-bootstrap/ListGroup";
+
 function App() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
-      
+      <Row>
+        <Col>
+          <aside>
+            <div className="ms-2 mt-2 d-flex justify-content-between">
+              <Button variant="secondary" onClick={handleShow}>
+                Search for places
+              </Button>
+
+              <Button variant="secondary" className="rounded-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-compass"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016zm6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
+                  <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z" />
+                </svg>
+              </Button>
+            </div>
+
+            <Offcanvas show={show} onHide={handleClose}>
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title></Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Form className="d-flex">
+                  <InputGroup className="me-2">
+                    <InputGroup.Text id="basic-addon1">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-search"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                      </svg>
+                    </InputGroup.Text>
+                    <Form.Control
+                      placeholder="search location"
+                      aria-label="Search"
+                      aria-describedby="basic-addon1"
+                    />
+                  </InputGroup>
+                  <Button variant="primary">Search</Button>
+                </Form>
+                <ListGroup variant="flush" className="mt-4">
+                  <ListGroup.Item
+                    action
+                    href="#link1"
+                    className="mb-3 d-flex justify-content-between"
+                  >
+                    <span>Zapopan</span>
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-chevron-right"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </span>
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    action
+                    href="#link2"
+                    className="mb-3 d-flex justify-content-between"
+                  >
+                    <span>Zapopan</span>
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-chevron-right"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </span>
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    action
+                    href="#link3"
+                    className="mb-3 d-flex justify-content-between"
+                  >
+                    <span>Zapopan</span>
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-chevron-right"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </span>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Offcanvas.Body>
+            </Offcanvas>
+          </aside>
+        </Col>
+        <Col md={9}>
+          <main>este es el main</main>
+        </Col>
+      </Row>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
